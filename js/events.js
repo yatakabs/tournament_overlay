dbe2048cba84b4c8d07ba84f3150fc81416b887b
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2025-05-14 23:42:02 +0900

    VSコードの整形機能で、空白タブなど一旦整形

--- js/events.js
+++ js/events.js
@@ -1,87 +1,87 @@
 const events = {
-	hello(data) {
-		console.log("Connected to Beat Saber");
-		ui.check_mark_hidden_set();
+  hello(data) {
+    console.log("Connected to Beat Saber");
+    ui.check_mark_hidden_set();
 
-		menu_shine = true;
-		if (data.status.beatmap && data.status.performance) {
-			ui.beatmap(data);
-			ui.performance(data);
-			ui.start_hidden_set();
-			ui.show();
-		}
-		if (typeof op_hello !== "undefined") op_hello(data);
-	},
+    menu_shine = true;
+    if (data.status.beatmap && data.status.performance) {
+      ui.beatmap(data);
+      ui.performance(data);
+      ui.start_hidden_set();
+      ui.show();
+    }
+    if (typeof op_hello !== "undefined") op_hello(data);
+  },
 
-	songStart(data) {
-		ui.beatmap(data);
-		ui.performance(data);
-		ui.start_hidden_set();
-		ui.show();
-		if (typeof op_songStart !== "undefined") op_songStart(data);
-	},
+  songStart(data) {
+    ui.beatmap(data);
+    ui.performance(data);
+    ui.start_hidden_set();
+    ui.show();
+    if (typeof op_songStart !== "undefined") op_songStart(data);
+  },
 
-	noteCut(data) {
-		ui.performance(data);
-		if (typeof op_noteCut !== "undefined") op_noteCut(data);
-	},
+  noteCut(data) {
+    ui.performance(data);
+    if (typeof op_noteCut !== "undefined") op_noteCut(data);
+  },
 
-	noteFullyCut(data) {
-		ui.performance(data);
-		if (typeof op_noteFullyCut !== "undefined") op_noteFullyCut(data);
-	},
+  noteFullyCut(data) {
+    ui.performance(data);
+    if (typeof op_noteFullyCut !== "undefined") op_noteFullyCut(data);
+  },
 
-	obstacleEnter(data) {
-		ui.performance(data);
-		if (typeof op_obstacleEnter !== "undefined") op_obstacleEnter(data);
-	},
+  obstacleEnter(data) {
+    ui.performance(data);
+    if (typeof op_obstacleEnter !== "undefined") op_obstacleEnter(data);
+  },
 
-	obstacleExit(data) {
-		ui.performance(data);
-		if (typeof op_obstacleExit !== "undefined") op_obstacleExit(data);
-	},
+  obstacleExit(data) {
+    ui.performance(data);
+    if (typeof op_obstacleExit !== "undefined") op_obstacleExit(data);
+  },
 
-	noteMissed(data) {
-		ui.performance(data);
-		if (typeof op_noteMissed !== "undefined") op_noteMissed(data);
-	},
+  noteMissed(data) {
+    ui.performance(data);
+    if (typeof op_noteMissed !== "undefined") op_noteMissed(data);
+  },
 
-	bombCut(data) {
-		ui.performance(data);
-		if (typeof op_bombCut !== "undefined") op_bombCut(data);
-	},
+  bombCut(data) {
+    ui.performance(data);
+    if (typeof op_bombCut !== "undefined") op_bombCut(data);
+  },
 
-	finished(data) {
-		ui.performance(data);
-		if (typeof op_finished !== "undefined") op_finished(data);
-	},
+  finished(data) {
+    ui.performance(data);
+    if (typeof op_finished !== "undefined") op_finished(data);
+  },
 
-	failed(data) {
-		ui.performance(data);
-		if (typeof op_failed !== "undefined") op_failed(data);
-	},
+  failed(data) {
+    ui.performance(data);
+    if (typeof op_failed !== "undefined") op_failed(data);
+  },
 
-	softFailed(data) {
-		ui.performance(data);
-	},
+  softFailed(data) {
+    ui.performance(data);
+  },
 
-	scoreChanged(data) {
-		ui.performance(data);
-		if (typeof op_scoreChanged !== "undefined") op_scoreChanged(data);
-	},
+  scoreChanged(data) {
+    ui.performance(data);
+    if (typeof op_scoreChanged !== "undefined") op_scoreChanged(data);
+  },
 
-	energyChanged(data) {
-		ui.performance(data);
-		if (typeof op_energyChanged !== "undefined") op_energyChanged(data);
-	},
+  energyChanged(data) {
+    ui.performance(data);
+    if (typeof op_energyChanged !== "undefined") op_energyChanged(data);
+  },
 
-	noteSpawned(data) {
-	},
+  noteSpawned(data) {
+  },
 
   other(data) {
     if (typeof data.other !== "undefined") {
       if (typeof data.other.HttpPlayButtonStatus !== "undefined") {
-        if (typeof data.other.HttpPlayButtonStatus.OptionScene === "undefined"){
+        if (typeof data.other.HttpPlayButtonStatus.OptionScene === "undefined") {
           if (typeof data.other.HttpPlayButtonStatus.SceneChange !== "undefined") obs_scene_change_enable = data.other.HttpPlayButtonStatus.SceneChange;
           if (typeof data.other.HttpPlayButtonStatus.PlayStart !== "undefined" && data.other.HttpPlayButtonStatus.PlayStart) {
             ui.start_hidden_set();
@@ -90,8 +90,8 @@ const events = {
       }
     }
   },
-  
-	beatmapEvent(data) {
+
+  beatmapEvent(data) {
     if (typeof data.status !== "undefined") {
       if (typeof data.status.performance !== "undefined") {
         var song_time = data.status.performance.currentSongTime;
@@ -104,40 +104,40 @@ const events = {
         }
       }
     }
-	},
-	
+  },
 
-	pause(data) {
-		var ip = query.get("ip");
-		var diff_time = 0;
-		if (ip && ip != "localhost" && ip != "127.0.0.1") {
-			diff_time = Date.now() - data.time;
-			console.log(diff_time);
-		}
-		ui.timer.pause(data.status.beatmap.paused + diff_time);
-		if (typeof op_pause !== "undefined") op_pause(data);
-	},
 
-	resume(data) {
-		ui.start_hidden_release1();
-		var ip = query.get("ip");
-		var diff_time = 0;
-		if (ip && ip != "localhost" && ip != "127.0.0.1") {
-			diff_time = Date.now() - data.time;
-			console.log(diff_time);
-		}
-		ui.timer.start(data.status.beatmap.start + diff_time, data.status.beatmap.length, false);
-		if (typeof op_resume !== "undefined") op_resume(data);
-	},
+  pause(data) {
+    var ip = query.get("ip");
+    var diff_time = 0;
+    if (ip && ip != "localhost" && ip != "127.0.0.1") {
+      diff_time = Date.now() - data.time;
+      console.log(diff_time);
+    }
+    ui.timer.pause(data.status.beatmap.paused + diff_time);
+    if (typeof op_pause !== "undefined") op_pause(data);
+  },
 
-	menu(data) {
-		menu_shine = true;
-		ui.start_hidden_release1();
-		ui.start_hidden_release2();
-		ui.timer.stop();
-		if (disp_hidden) {
-			ui.hide();
-		}
-		if (typeof op_menu !== "undefined") op_menu(data);
-	}
+  resume(data) {
+    ui.start_hidden_release1();
+    var ip = query.get("ip");
+    var diff_time = 0;
+    if (ip && ip != "localhost" && ip != "127.0.0.1") {
+      diff_time = Date.now() - data.time;
+      console.log(diff_time);
+    }
+    ui.timer.start(data.status.beatmap.start + diff_time, data.status.beatmap.length, false);
+    if (typeof op_resume !== "undefined") op_resume(data);
+  },
+
+  menu(data) {
+    menu_shine = true;
+    ui.start_hidden_release1();
+    ui.start_hidden_release2();
+    ui.timer.stop();
+    if (disp_hidden) {
+      ui.hide();
+    }
+    if (typeof op_menu !== "undefined") op_menu(data);
+  }
 }
--- js/options.js
+++ js/options.js
@@ -6,62 +6,62 @@ var auto_scale = true;
 var start_hidden1 = false;
 var start_hidden2 = false;
 var menu_shine = true;
-const check_id = ["overlay","rank","percentage","combo","score","progress","mod_nf","raw_score",
-                  "image","title","subtitle","artist","difficulty","bpm","njs","bsr","bsr_text",
-                  "mapper","mapper_header","mapper_footer","song_time","song_length","mod","miss",
-                  "pre_bsr","pre_bsr_text","njs_text","energy","energy_bar","energy_group"]
+const check_id = ["overlay", "rank", "percentage", "combo", "score", "progress", "mod_nf", "raw_score",
+  "image", "title", "subtitle", "artist", "difficulty", "bpm", "njs", "bsr", "bsr_text",
+  "mapper", "mapper_header", "mapper_footer", "song_time", "song_length", "mod", "miss",
+  "pre_bsr", "pre_bsr_text", "njs_text", "energy", "energy_bar", "energy_group"]
 var html_id = {};
 for (var i = 0, len = check_id.length; i < len; ++i) {
-	if (document.getElementById(check_id[i]) === null) {
-		html_id[check_id[i]] = false;
-	} else {
-		html_id[check_id[i]] = true;
-	}
+  if (document.getElementById(check_id[i]) === null) {
+    html_id[check_id[i]] = false;
+  } else {
+    html_id[check_id[i]] = true;
+  }
 }
 if (html_id["mapper_header"]) var mapper_header_org = document.getElementById("mapper_header").textContent;
 if (html_id["mapper_footer"]) var mapper_footer_org = document.getElementById("mapper_footer").textContent;
-if (html_id["bsr_text"])      var bsr_text_org = document.getElementById("bsr_text").textContent;
-if (html_id["pre_bsr_text"])  var pre_bsr_text_org = document.getElementById("pre_bsr_text").textContent;
-if (html_id["njs_text"])      var njs_text_org = document.getElementById("njs_text").textContent;
+if (html_id["bsr_text"]) var bsr_text_org = document.getElementById("bsr_text").textContent;
+if (html_id["pre_bsr_text"]) var pre_bsr_text_org = document.getElementById("pre_bsr_text").textContent;
+if (html_id["njs_text"]) var njs_text_org = document.getElementById("njs_text").textContent;
 
 (() => {
-	const handlers = {
-		modifiers(string) {
-			string.split(",").forEach((modifier) => {
-				if (modifier === "no-hidden") {
-					disp_hidden = false;
-					if (html_id["overlay"]) document.getElementById("overlay").classList.remove("hidden");
-					return;
-				}
-				if (modifier === "bsr") {
-					bsr_display = true;
-				}
+  const handlers = {
+    modifiers(string) {
+      string.split(",").forEach((modifier) => {
+        if (modifier === "no-hidden") {
+          disp_hidden = false;
+          if (html_id["overlay"]) document.getElementById("overlay").classList.remove("hidden");
+          return;
+        }
+        if (modifier === "bsr") {
+          bsr_display = true;
+        }
         if (modifier === "scale") {
           auto_scale = false;
         }
-				var link = document.createElement("link");
-				
-				link.setAttribute("rel", "stylesheet");
-				link.setAttribute("href", `./modifiers/${modifier}.css`);
-		
-				document.head.appendChild(link);
-			});
-		}
-	};
+        var link = document.createElement("link");
 
-	Object.keys(handlers).forEach((key) => {
-		var value = query.get(key);
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("href", `./modifiers/${modifier}.css`);
 
-		if (value) {
-			handlers[key](value);
-		}
-	});
-	
-	if (location.hash) {
-		// Legacy URL hash support
-		handlers.modifiers(location.hash.slice(1));
-	}
-  
+        document.head.appendChild(link);
+      });
+    }
+  };
+
+  Object.keys(handlers).forEach((key) => {
+    var value = query.get(key);
+
+    if (value) {
+      handlers[key](value);
+    }
+  });
+
+  if (location.hash) {
+    // Legacy URL hash support
+    handlers.modifiers(location.hash.slice(1));
+  }
+
   if (auto_scale) {
     document.documentElement.style.zoom = document.documentElement.offsetWidth / 1280;
   }
